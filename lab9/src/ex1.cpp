e81036a495d8e126bffae736ce9872281192cbb9
rosi k <rosik22@abv.bg>
2020-02-19 21:33:18 +0200

    lab9

--- lab9/src/ex1.cpp
+++ lab9/src/ex1.cpp
@@ -4,6 +4,30 @@
 
 using namespace std;
 
+char opening(char c){
+    return c == ')' ? '(':'[';
+}
+
+bool valid (string line){
+    stack<char> ans;
+    char c;
+    for(int i=0; i<line.length(); i++){
+        c = line[i];
+        if(c == '(' || c == '['){
+            ans.push(c);
+        }
+        else if(c == ')' || c == ']'){
+            if(ans.top() == opening(c)) ans.pop();
+            else return false;
+        }
+    }
+    return ans.empty();
+}
+
 int main(int argc, char *argv[]) {
+    string line;
+    while(getline(cin,line)){
+        cout<<(valid(line)? "Yes" : "No")<<endl;
+    }
     return 0;
 }
--- lab9/src/ex2.cpp
+++ lab9/src/ex2.cpp
@@ -6,6 +6,27 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) return -1;
+
+    map<string, int> ans;
+    int i = argc - 1;
+    string word;
+    while(i>0){
+        word = argv[i];
+        map<string, int>::iterator itr = ans.find(word);
+        if(itr != ans.end())
+            ans[word] = ans[word] + 1;
+        else ans.insert(pair<string,int>(word,1));
+        i--;
+    }
+
+    map<string, int>::iterator best = ans.begin();
+
+    for(auto it = ans.begin(); it != ans.end(); it++){
+        if(it->second > best->second) best = it;
+    }
+
+    cout<<best->first<<" "<<best->second<<endl;
 
     return 0;
 }
